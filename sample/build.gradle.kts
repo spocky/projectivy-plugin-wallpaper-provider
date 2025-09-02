30ae65edfa785a6581b162b7da2d5afbca8a31f9
spocky <spocky12+dev@gmail.com>
2025-09-03 01:39:25 +0200

    Updated dependencies

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -36,10 +36,14 @@ android {
             jvmTarget.set(JvmTarget.JVM_17)
         }
     }
+
+    buildFeatures {
+        buildConfig = true
+    }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.leanback:leanback:1.2.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.12.0")
