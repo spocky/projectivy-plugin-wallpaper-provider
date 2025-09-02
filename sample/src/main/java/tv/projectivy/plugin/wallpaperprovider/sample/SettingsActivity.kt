3e00a629fb0efc1b9773dae1b9421030b0ad1279
spocky <spocky12+dev@gmail.com>
2025-09-03 01:38:16 +0200

    Check Projectivy Launcher presence on the device

--- sample/src/main/java/tv/projectivy/plugin/wallpaperprovider/sample/SettingsActivity.kt
+++ sample/src/main/java/tv/projectivy/plugin/wallpaperprovider/sample/SettingsActivity.kt
@@ -1,6 +1,8 @@
 package tv.projectivy.plugin.wallpaperprovider.sample
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
 import androidx.leanback.app.GuidedStepSupportFragment
 
@@ -9,9 +11,22 @@ class SettingsActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        if (!packageManager.isApplicationInstalled("com.spocky.projengmenu")) {
+            Toast.makeText(this, R.string.projectivy_not_installed, Toast.LENGTH_LONG).show()
+        }
+
         val fragment: GuidedStepSupportFragment = SettingsFragment()
         if (savedInstanceState == null) {
             GuidedStepSupportFragment.addAsRoot(this, fragment, android.R.id.content)
+        }
+    }
+
+    fun PackageManager.isApplicationInstalled(packageName: String): Boolean {
+        return try {
+            getApplicationInfo(packageName, 0)
+            true
+        } catch (_: PackageManager.NameNotFoundException) {
+            false
         }
     }
 }
