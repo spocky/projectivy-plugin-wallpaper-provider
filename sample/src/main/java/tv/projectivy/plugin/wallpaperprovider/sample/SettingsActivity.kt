cad2f9942ffb342a62f526f32da1c2695ddd5531
spocky <spocky12+dev@gmail.com>
2026-01-11 01:25:02 +0100

    Added ability to request a wallpaper refresh

--- sample/src/main/java/tv/projectivy/plugin/wallpaperprovider/sample/SettingsActivity.kt
+++ sample/src/main/java/tv/projectivy/plugin/wallpaperprovider/sample/SettingsActivity.kt
@@ -1,17 +1,19 @@
 package tv.projectivy.plugin.wallpaperprovider.sample
 
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
 import androidx.leanback.app.GuidedStepSupportFragment
+import tv.projectivy.plugin.wallpaperprovider.api.WallpaperProviderContract
 
 class SettingsActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (!packageManager.isApplicationInstalled("com.spocky.projengmenu")) {
+        if (!packageManager.isApplicationInstalled(PROJECTIVY_PACKAGE_ID)) {
             Toast.makeText(this, R.string.projectivy_not_installed, Toast.LENGTH_LONG).show()
         }
 
@@ -21,6 +23,20 @@ class SettingsActivity : FragmentActivity() {
         }
     }
 
+
+    // Call this method to inform Projectivy that it needs to request new wallpapers because the settings have changed
+    // Note : if the settings have been opened from Projectivy Settings>Appearance>Wallpaper, this call is not needed.
+    fun requestWallpaperUpdate() {
+        val intent = Intent(WallpaperProviderContract.ACTION_WALLPAPER_PROVIDER_UPDATED).apply {
+            `package` = PROJECTIVY_PACKAGE_ID
+
+            putExtra(WallpaperProviderContract.EXTRA_PROVIDER_ID, R.string.plugin_uuid)
+            putExtra(WallpaperProviderContract.EXTRA_UPDATE_REASON, WallpaperProviderContract.UpdateReason.PREFS_CHANGED)
+        }
+
+        sendBroadcast(intent)
+    }
+
     fun PackageManager.isApplicationInstalled(packageName: String): Boolean {
         return try {
             getApplicationInfo(packageName, 0)
@@ -28,5 +44,9 @@ class SettingsActivity : FragmentActivity() {
         } catch (_: PackageManager.NameNotFoundException) {
             false
         }
+    }
+
+    companion object {
+        private const val PROJECTIVY_PACKAGE_ID = "com.spocky.projengmenu"
     }
 }
